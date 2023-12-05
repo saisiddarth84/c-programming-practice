ec92f749c4569e4131d4a0d2b4a6245c4d5791be
saisiddarth84 <145665427+saisiddarth84@users.noreply.github.com>
2023-12-05 17:31:07 +0530

    Create 1_program.c

---
+++ operators/1_program.c
@@ -0,0 +1,9 @@
+// 1. WAP to calculate size of a real constant.
+#include<stdio.h>
+int main()
+{
+    int a;
+    a = sizeof(23.9);
+    printf("The size of the real constant is %d",a);
+    return 0;
+}
