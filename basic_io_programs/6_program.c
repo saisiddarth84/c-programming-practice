efadb564af47f83b7b6479f8660fe2012495576a
saisiddarth84 <145665427+saisiddarth84@users.noreply.github.com>
2023-12-07 12:10:43 +0530

    Create 6_program.c

---
+++ basic_io_programs/6_program.c
@@ -0,0 +1,10 @@
+// 1.WAP to input a character from the user and print its ASCII code.
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character:\n");
+    scanf("%c",&ch);
+    printf("%c %d",ch,ch);
+    return 0;
+}
