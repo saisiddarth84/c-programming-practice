1bf2132287a4ff9c6dd41c520fa5b7dadf578232
saisiddarth84 <145665427+saisiddarth84@users.noreply.github.com>
2023-12-07 12:12:07 +0530

    Create 7_program.c

---
+++ basic_io_programs/7_program.c
@@ -0,0 +1,10 @@
+// 2. WAP to input a ASCII code from the user and print its corresponding character.
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a ASCII code:\n");
+    scanf("%d",&a);
+    printf("%d %c",a,a);
+    return 0;
+}
