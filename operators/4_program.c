9f6789f9ecf11994246734d11df0b82ee23ba4f3
saisiddarth84 <145665427+saisiddarth84@users.noreply.github.com>
2023-12-08 15:03:56 +0530

    Update 4_program.c

--- operators/4_program.c
+++ operators/4_program.c
@@ -1,8 +1,8 @@
-4.WAP to swap values of two int variables without using third variable.
+4.WAP to swap values of two int variables without Using third variable.
 #include<stdio.h>
 int main(){
     int a,b;
-    a=20, b=30;
+    scanf("%d%d",&a,&b);
     printf("a=%d, b=%d\n",a,b);
     a = a + b;           
     b = a - b;          
