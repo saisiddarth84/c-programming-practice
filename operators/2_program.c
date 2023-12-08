7d7e978a89cf59fb4a6a8727641e46eaf9fbba3a
saisiddarth84 <145665427+saisiddarth84@users.noreply.github.com>
2023-12-08 14:38:30 +0530

    Create 2_program.c

---
+++ operators/2_program.c
@@ -0,0 +1,9 @@
+// 3. WAP with one char type variable.Assign 'A' in the ariable. Now change the value from 'A' to 'B' using increment operator.
+/#include<stdio.h>
+int main()
+{
+    char x='A';
+    printf("%c",++x); 
+    return 0; 
+}
+
